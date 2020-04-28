49d975c54b20e33a1020e60d3799d97d916718ca
Jorge P. Júnior <jorgejunior.jpj2016@gmail.com>
2020-04-28 18:09:07 -0300

    Algumas adições de arquivos no backend e no frontend

--- frontend/src/screens/department/NewDpt/index.js
+++ frontend/src/screens/department/NewDpt/index.js
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './styles.css'
 
+import api from '../../../services/api'
+
 export default function NewDpt() {
+    const [nomeDepartamento, setNome] = useState('')
+    
+    async function effectNewDpt(e) {
+        e.preventDefault()
+
+        const data = {
+            nomeDepartamento
+        }
+
+        try {
+           const response = await api.post('depts', data)
+
+            alert(`Código do departamento: ${response.data.codDepartamento}`)
+        } catch (error) {
+            alert('Não foi possível cadastrar!!')
+        }
+    }
 
     return (
         <div className="department-container">
             <h1>Cadastrar Novo Departamento</h1>
 
-            <form>
-                <input placeholder="Nome do Departamento" />
+            <form onSubmit={effectNewDpt}>
+                <input 
+                    placeholder="Nome do Departamento" 
+                    value={nomeDepartamento}
+                    onChange={e => setNome(e.target.value)}
+                />
                 <button type="submit">Salvar</button>
             </form>
         </div>
